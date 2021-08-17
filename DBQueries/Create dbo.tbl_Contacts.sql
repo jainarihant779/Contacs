75572501bf39cdbb674e349d6da1b934cf3d3e47
jainarihant779 <89023640+jainarihant779@users.noreply.github.com>
2021-08-17 11:15:55 +0530

    Add files via upload

---
+++ DBQueries/Create dbo.tbl_Contacts.sql
@@ -0,0 +1,20 @@
+﻿USE [ManageContacts]
+GO
+
+/****** Object: Table [dbo].[tbl_Contacts] Script Date: 16-08-2021 18:58:03 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbl_Contacts] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (20)  NOT NULL,
+    [LastName]    VARCHAR (100) NOT NULL,
+    [Email]       VARCHAR (20)  NOT NULL,
+    [PhoneNumber] VARCHAR (15)  NOT NULL,
+    [status]      BIT           NOT NULL
+);
+
+
